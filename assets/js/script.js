3f540c850787889e1df29335c6816bebaeb48415
thisisgene <emdo2000@gmail.com>
2017-06-27 19:30:23 +0200

    Scroll on page now working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,37 @@ $(window).scroll( function () {
     $('section#home').removeClass('minified');
   }
 });
+
+$.mark = {
+  jump: function (options) {
+    var defaults = {
+      selector: 'a.scroll-on-page-link'
+    };
+    if (typeof options === 'string') {
+      defaults.selector = options;
+    }
+
+    options = $.extend(defaults, options);
+    return $(options.selector).click(function (e) {
+      var jumpobj = $(this);
+      var target = jumpobj.attr('href');
+      var thespeed = 1000;
+      var offset = $(target).offset().top;
+      $('.link--active').removeClass('link--active');
+      jumpobj.addClass('link--active');
+      // clickToActivate = true;
+
+      $('html,body').animate({
+        scrollTop: offset
+      }, thespeed, 'swing', function(){
+        // setTimeout(function(){clickToActivate = false;}, 100);
+      });
+      e.preventDefault();
+    });
+  }
+};
+
+
+$(document).ready(function() {
+  $.mark.jump();
+});
