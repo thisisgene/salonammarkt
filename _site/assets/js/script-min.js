60de7c17228cc5ce92ab9c192aa207aca2cc4514
Eugene Delta <eugenedelta@Eugenes-MacBook-Air.local>
2024-01-29 11:38:13 +0100

    Neuer Text.

--- _site/assets/js/script-min.js
+++ _site/assets/js/script-min.js
@@ -4,87 +4,84 @@ var lastScrollTop = 0
 var scrollDirection
 var clickToActivate
 
-var imgCount = 5
+var imgCount = 10
 var currentSlide = 1
 var slideInterval = 3000
 var stopShow = false
 var passedTime = 0
 var setStop
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   scrollTop = $(this).scrollTop()
 
   if (scrollTop > lastScrollTop) {
-    scrollDirection = 'down'
+    scrollDirection = "down"
   } else {
-    scrollDirection = 'up'
+    scrollDirection = "up"
   }
   lastScrollTop = scrollTop
-  console.log(1 - scrollTop / 100)
-  $('.arrow-down').css('opacity', 1 - scrollTop / 100)
+  $(".arrow-down").css("opacity", 1 - scrollTop / 100)
   if (
-    scrollTop > $('.home-wrapper').offset().top &&
-    scrollDirection === 'down'
+    scrollTop > $(".home-wrapper").offset().top &&
+    scrollDirection === "down"
   ) {
-    if (!$('section#home').hasClass('minified')) {
-      $('section#home').addClass('minified')
-      setTimeout(function() {
-        $('section#home').addClass('dir-change')
+    if (!$("section#home").hasClass("minified")) {
+      $("section#home").addClass("minified")
+      setTimeout(function () {
+        $("section#home").addClass("dir-change")
       }, 200)
       currentTop = scrollTop
     }
   }
   if (scrollTop < currentTop) {
-    $('section#home').removeClass('minified dir-change')
+    $("section#home").removeClass("minified dir-change")
   }
 })
 
 $.mark = {
-  jump: function(options) {
+  jump: function (options) {
     var defaults = {
-      selector: 'a.scroll-on-page-link'
+      selector: "a.scroll-on-page-link",
     }
-    if (typeof options === 'string') {
+    if (typeof options === "string") {
       defaults.selector = options
     }
 
     options = $.extend(defaults, options)
-    return $(options.selector).click(function(e) {
+    return $(options.selector).click(function (e) {
       var jumpobj = $(this)
-      var target = jumpobj.attr('href')
+      var target = jumpobj.attr("href")
       var thespeed = 1000
       var offset = $(target).offset().top
-      $('.link--active').removeClass('link--active')
-      jumpobj.addClass('link--active')
+      $(".link--active").removeClass("link--active")
+      jumpobj.addClass("link--active")
       clickToActivate = true
 
-      $('html,body').animate(
+      $("html,body").animate(
         {
-          scrollTop: offset
+          scrollTop: offset,
         },
         thespeed,
-        'swing',
-        function() {
+        "swing",
+        function () {
           // setTimeout(function(){clickToActivate = false;}, 100);
         }
       )
       e.preventDefault()
     })
-  }
+  },
 }
 
 function slideShow(startCount) {
   currentSlide = startCount
-  $('.active-slide')
-    .removeClass('active-slide')
-    .css('opacity', '0')
-  $('.gallery-list li:nth-child(' + currentSlide + ') span')
-    .addClass('active-slide')
-    .css('opacity', '1')
+  $(".active-slide").removeClass("active-slide").css("opacity", "0")
+  $(".gallery-list li:nth-child(" + currentSlide + ") span")
+    .addClass("active-slide")
+    .css("opacity", "1")
 }
 
 function slideLoop() {
-  setInterval(function() {
+  setInterval(function () {
     if (stopShow) {
     } else {
       if (currentSlide < imgCount) {
@@ -101,43 +98,39 @@ function changeSlide(direction) {
   stopShow = true
   stopStop = false
   clearTimeout(setStop)
-  if (direction === 'next') {
+  if (direction === "next") {
     if (currentSlide < imgCount) {
       currentSlide = currentSlide + 1
     } else {
       currentSlide = 1
     }
-    $('.active-slide')
-      .removeClass('active-slide')
-      .css('opacity', '0')
-    $('.gallery-list li:nth-child(' + currentSlide + ') span')
-      .addClass('active-slide')
-      .css('opacity', '1')
+    $(".active-slide").removeClass("active-slide").css("opacity", "0")
+    $(".gallery-list li:nth-child(" + currentSlide + ") span")
+      .addClass("active-slide")
+      .css("opacity", "1")
   }
-  if (direction === 'back') {
+  if (direction === "back") {
     if (currentSlide > 1) {
       currentSlide = currentSlide - 1
     } else {
       currentSlide = imgCount
     }
-    $('.active-slide')
-      .removeClass('active-slide')
-      .css('opacity', '0')
-    $('.gallery-list li:nth-child(' + currentSlide + ') span')
-      .addClass('active-slide')
-      .css('opacity', '1')
+    $(".active-slide").removeClass("active-slide").css("opacity", "0")
+    $(".gallery-list li:nth-child(" + currentSlide + ") span")
+      .addClass("active-slide")
+      .css("opacity", "1")
   }
 
-  setStop = setTimeout(function() {
+  setStop = setTimeout(function () {
     stopShow = false
   }, 10000)
 }
 
-$(window).on('beforeunload', function() {
+$(window).on("beforeunload", function () {
   $(window).scrollTop(0)
 })
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(this).scrollTop(0)
   $.mark.jump()
 
@@ -145,57 +138,62 @@ $(document).ready(function() {
 })
 
 function initMap() {
-  var mapMarker = '../assets/img/map-marker.png'
+  var mapMarker = "../assets/img/map-marker.png"
   var salonammarkt = { lat: 48.2172205, lng: 16.3779313 }
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
     center: salonammarkt,
     scrollwheel: false,
     styles: [
       {
-        featureType: 'administrative',
-        elementType: 'geometry',
-        stylers: [{ visibility: 'off' }]
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [{ visibility: "off" }],
       },
       {
-        featureType: 'landscape',
-        elementType: 'geometry.fill',
-        stylers: [{ color: '#fff3de' }, { saturation: 95 }, { weight: 4.5 }]
+        featureType: "landscape",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#fff3de" }, { saturation: 95 }, { weight: 4.5 }],
       },
       {
-        featureType: 'landscape',
-        elementType: 'geometry.stroke',
-        stylers: [{ visibility: 'simplified' }]
+        featureType: "landscape",
+        elementType: "geometry.stroke",
+        stylers: [{ visibility: "simplified" }],
       },
       {
-        featureType: 'landscape.natural',
-        elementType: 'geometry.fill',
-        stylers: [{ color: '#2dff16' }]
+        featureType: "landscape.natural",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#2dff16" }],
       },
-      { featureType: 'poi', stylers: [{ visibility: 'off' }] },
+      { featureType: "poi", stylers: [{ visibility: "off" }] },
       {
-        featureType: 'road',
-        elementType: 'labels.icon',
-        stylers: [{ visibility: 'off' }]
+        featureType: "road",
+        elementType: "labels.icon",
+        stylers: [{ visibility: "off" }],
       },
-      { featureType: 'transit', stylers: [{ visibility: 'off' }] }
-    ]
+      { featureType: "transit", stylers: [{ visibility: "off" }] },
+    ],
   })
   var marker = new google.maps.Marker({
     position: salonammarkt,
     map: map,
     icon: mapMarker,
-    size: new google.maps.Size(20, 32)
+    size: new google.maps.Size(20, 32),
   })
 }
 
 function showEmailAlert() {
-  $('.email-alert-container').show(3000)
+  $(".email-alert-container").show(3000)
 }
 function hideEmailAlert() {
-  $('.email-alert-container').hide()
+  $(".email-alert-container").hide()
 }
 
-function hideAktionAlert() {
-  $('.aktion-alert-container').fadeOut()
+function hideAktionAlert(e) {
+  $(".aktion-alert-container").fadeOut()
 }
+
+$("#close-button").click(function (e) {
+  e.preventDefault()
+  $(".aktion-alert-container").fadeOut()
+})
